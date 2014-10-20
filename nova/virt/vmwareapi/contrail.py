ab8039ea2e9c226ea2a28e9267b0f50ef7d0ed1f
Divakar <ddivakar@juniper.net>
2014-10-20 01:55:43 -0700

    Bug:#1381980. Changing the vmware nova compute plugin to use vrouter_api
    Change-Id: I513a8b5b0d65b0ad3114c614047bede7d55acd04

--- nova/virt/vmwareapi/contrail.py
+++ nova/virt/vmwareapi/contrail.py
@@ -47,8 +47,8 @@ from bitstring import BitArray
 from thrift.transport import TTransport, TSocket
 from thrift.transport.TTransport import TTransportException
 from thrift.protocol import TBinaryProtocol, TProtocol
-from nova_contrail_vif.gen_py.instance_service import InstanceService
-from nova_contrail_vif.gen_py.instance_service import ttypes
+from contrail_vrouter_api.vrouter_api import ContrailVRouterApi
+from thrift.Thrift import TApplicationException
 
 LOG = logging.getLogger(__name__)
 
@@ -61,99 +61,18 @@ CONF.register_opts(vmwareapi_contrail_opts, 'vmware')
 
 class ContrailVIFDriver(object):
     """to inform agent"""
+
+    PORT_TYPE = 'NovaVMPort'
+
     def __init__(self):
         super(ContrailVIFDriver, self).__init__()
-        self._agent_alive = False
-        self._agent_connected = False
-        self._port_dict = {}
-        self._protocol = None
+        self._vrouter_client = ContrailVRouterApi()
         timer = loopingcall.FixedIntervalLoopingCall(self._keep_alive)
         timer.start(interval=2)
     #end __init__
 
-    def _agent_connect(self, protocol):
-        # Agent connect for first time
-        #if self._agent_connected == False and protocol != None:
-        if protocol != None:
-            service = InstanceService.Client(protocol)
-            return service.Connect()
-        else:
-            return False
-    #end __agent_connect
-
     def _keep_alive(self):
-        try:
-            if self._agent_alive == False:
-                self._protocol = self._agent_conn_open()
-                if self._protocol == None:
-                    return
-
-            service = InstanceService.Client(self._protocol)
-            aa_latest = service.KeepAliveCheck()
-            if self._agent_alive == False and aa_latest == True:
-                port_l = [v for k, v in self._port_dict.iteritems()]
-                LOG.debug(('Agent sending port list %d, %s'), len(port_l), self)
-                if len(port_l):
-                    for i in range(len(port_l)):
-                        LOG.debug(('Port %s %s'), port_l[i].tap_name, port_l[i].ip_address)
-
-                service.AddPort(port_l)
-
-                self._agent_alive = True
-
-            if self._agent_alive == True and aa_latest == False:
-                LOG.debug(('Agent not available, %s'), self)
-                self._agent_alive = False
-                return
-
-        except:
-            self._agent_alive = False
-            LOG.debug(('Agent keep alive exception: %s'), self)
-    #end _keep_alive
-
-    def _agent_conn_open(self):
-        try:
-            socket = TSocket.TSocket("127.0.0.1", 9090)
-            transport = TTransport.TFramedTransport(socket)
-            transport.open()
-            protocol = TBinaryProtocol.TBinaryProtocol(transport)
-            self._agent_connected = self._agent_connect(protocol)
-            return protocol
-        except TTransportException:
-            return None
-    #end _agent_conn_open
-
-    def _convert_to_bl(self, id):
-        hexstr = uuid.UUID(id).hex
-        return [int(hexstr[i:i+2], 16) for i in range(32) if i%2 == 0]
-    #end _convert_to_bl
-
-    def _agent_inform(self, port, id, add):
-        # First add to the port list
-        if add == True:
-            self._port_dict[id] = port
-        else:
-            if id in self._port_dict:
-                del self._port_dict[id]
-
-        if not self._agent_alive:
-            return
-
-        LOG.debug(('agent_inform %s, %s, %s, %s'),
-                  port.ip_address,
-                  port.tap_name,
-                  add,
-                  self)
-        try:
-            service = InstanceService.Client(self._protocol)
-            if add == True:
-                service.AddPort([port])
-            else:
-                service.DeletePort(port.port_id)
-        except:
-            self._agent_alive = False
-
-    #end _agent_inform
+        self._vrouter_client.periodic_connection_check()
 
     def get_config(self, instance, network, mapping, image_meta):
         conf = super(VRouterVIFDriver, self).get_config(instance, network, mapping, image_meta)
@@ -168,46 +87,42 @@ class ContrailVIFDriver(object):
         subnets_v4 = [s for s in network['subnets'] if s['version'] == 4]
         if len(subnets_v4[0]['ips']) > 0:
             ip = subnets_v4[0]['ips'][0]['address']
+        kwargs = {
+            'ip_address': ip,
+            'vn_id': vif['network']['id'],
+            'display_name': instance['display_name'],
+            'hostname': instance['hostname'],
+            'host': instance['host'],
+            'vm_project_id': instance['project_id'],
+            'port_type': self.PORT_TYPE,
+            'ip6_address': None,
+            'vlan' : vlan_id,
+        }
+        try:
+            result = self._vrouter_client.add_port(instance['uuid'],
+                                                   vif['id'],
+                                                   network['bridge'],
+                                                   vif['address'],
+                                                   **kwargs)
+            if not result:
+                LOG.exception(_LE("Failed while plugging vif"),
+                              instance=instance)
+        except TApplicationException:
+            LOG.exception(_LE("Failed while plugging vif"), instance=instance)
 
-        # port_id(tuuid), instance_id(tuuid), tap_name(string), 
-        # ip_address(string), vn_id(tuuid)
-        port = ttypes.Port(self._convert_to_bl(vif['id']),
-                           self._convert_to_bl(instance['uuid']),
-                           network['bridge'],
-                           ip,
-                           self._convert_to_bl(vif['network']['id']),
-                           vif['address'],
-                           instance['display_name'],
-                           instance['hostname'],
-                           instance['host'],
-                           self._convert_to_bl(instance['project_id']),
-                           vlan_id)
-        self._agent_inform(port, vif['id'], True)
     #end plug
 
     def unplug(self, instance, vif):
         """Unplug the VIF from the network by deleting the port from
         the bridge."""
-        ip = 0
         if not vif:
             return
-
-        network = vif['network']
-        subnets_v4 = [s for s in network['subnets'] if s['version'] == 4]
-        if len(subnets_v4[0]['ips']) > 0:
-            ip = subnets_v4[0]['ips'][0]['address']
-
-        port = ttypes.Port(self._convert_to_bl(vif['id']),
-                           self._convert_to_bl(instance['uuid']),
-                           network['bridge'],
-                           ip,
-                           self._convert_to_bl(vif['network']['id']),
-                           vif['address'],
-                           instance['display_name'],
-                           instance['hostname'],
-                           instance['host'])
-        self._agent_inform(port, vif['id'], False)
-
+        try:
+            self._vrouter_client.delete_port(vif['id'])
+        except (TApplicationException, processutils.ProcessExecutionError,\
+        RuntimeError):
+            LOG.exception(_LE("Failed while unplugging vif"),
+                          instance=instance)
 
 
 INVALID_VLAN_ID = 4096
